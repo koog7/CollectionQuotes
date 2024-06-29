d518eb8fae1af189cc9e21f107dd6b9fae76e3bd
koog7 <k.ognev709@mail.com>
2024-06-29 11:48:13 +0600

    Создан навбар с роутами. Созданы контейнеры главной страницы и формы создания.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import {NavLink, Route, Routes} from "react-router-dom";
+import Home from "./containers/Home.tsx";
 
-const App = () => (
-    <>
-
-    </>
-);
+const App = () => {
+    return(
+        <>
+            <div>
+                <div className={'navbar'}>
+                    <h2>Quotes Central</h2>
+                    <div>
+                        <NavLink className="nav-link" to="/">Quotes</NavLink>
+                        <NavLink className="nav-link" to="/new-quote">Submit new quotes</NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route path="/" element={(
+                        <Home />
+                    )}/>
+                </Routes>
+            </div>
+        </>
+    );
+};
 
 export default App
