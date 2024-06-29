f7521b67a14f566202f0fc4f64e84cc26f9ab981
koog7 <k.ognev709@mail.com>
2024-06-29 15:42:28 +0600

    Добавлена функция удаления цитаты из бд и стейтаÑ

--- src/containers/Quotes.tsx
+++ src/containers/Quotes.tsx
@@ -5,38 +5,65 @@ import axiosAPI from '../axios/axiosLink.tsx'
 import {Button, Card, CardContent, Typography} from "@mui/material";
 
 interface FormData  {
+    id: string;
     quoteCategory: string;
     author: string;
     quote: string;
 }
 
 const Quotes = () => {
-    const [quotes, setQuotes] = useState<FormData | null>(null)
+    const [quotes, setQuotes] = useState<FormData[]>([])
     const {category} = useParams()
     useEffect(() => {
         if(category !== 'all'){
             axiosAPI.get(`/quotes.json?orderBy="quoteCategory"&equalTo="${category}"`)
                 .then(response => {
-                    setQuotes(response.data);
+                    const quotesArray = Object.keys(response.data).map(key => ({
+                        id: key,
+                        ...response.data[key]
+                    }));
+                    setQuotes(quotesArray);
                     console.log(response.data)
                 });
         }else{
             axiosAPI.get('/quotes.json')
                 .then(response => {
-                    setQuotes(response.data);
+                    const quotesArray = Object.keys(response.data).map(key => ({
+                        id: key,
+                        ...response.data[key]
+                    }));
+                    setQuotes(quotesArray);
                     console.log(response.data)
                 });
         }
     }, [category]);
+    console.log(quotes)
+    const deleteQuote = async (quoteId: string) => {
+        try {
+            await axiosAPI.delete(`/quotes/${quoteId}.json`);
+            setQuotes(prevQuotes => prevQuotes.filter(quote => quote.id !== quoteId));
+        }catch (error) {
+            console.error('Error deleting quote:', error);
+        }
+    }
+
     return (
         <div style={{display:"flex", flexDirection: 'row'}}>
             <Home />
             <div>
-                {quotes ? (
+                {quotes.length > 0 ? (
                     <div>
-                        {Object.entries(quotes).map(([key, quote]) => (
-                            <div key={key} className="quote-card">
-                                <Card className="quote-card" style={{width:'500px' , marginLeft:'300px',marginTop:'20px', textAlign:'left',display:"flex", flexDirection:'row'}}>
+                        <h1 style={{textAlign: 'center'}}>{category}</h1>
+                        {quotes.map(quote => (
+                            <div key={quote.id} className="quote-card">
+                                <Card className="quote-card" style={{
+                                    width: '500px',
+                                    marginLeft: '300px',
+                                    marginTop: '20px',
+                                    textAlign: 'left',
+                                    display: "flex",
+                                    flexDirection: 'row'
+                                }}>
                                     <CardContent>
                                         <Typography variant="body1" component="p">
                                             "{quote.quote}"
@@ -45,18 +72,20 @@ const Quotes = () => {
                                             - {quote.author}
                                         </Typography>
                                     </CardContent>
-                                    <CardContent sx={{marginLeft:'auto',display:"flex", gap:'5px'}}>
-                                        <Button variant="outlined" sx={{ color: 'green', borderColor: 'green' }}>Edit</Button>
-                                        <Button variant="outlined" sx={{ color: 'red', borderColor: 'red' }}>Delete</Button>
+                                    <CardContent sx={{marginLeft: 'auto', display: "flex", gap: '5px'}}>
+                                        <Button variant="outlined"
+                                                sx={{color: 'green', borderColor: 'green'}}>Edit</Button>
+                                        <Button variant="outlined" sx={{color: 'red', borderColor: 'red'}}
+                                                onClick={() => deleteQuote(quote.id)}>Delete</Button>
                                     </CardContent>
                                 </Card>
                             </div>
                         ))}
                     </div>
-                ):(
-                    <div>
-                        <h1>There is nothing...</h1>
-                    </div>
+                ) : (
+                    <>
+
+                    </>
                 )}
             </div>
         </div>
