34b9f9eb5b71caa6c04e926f0b98e50cf78af968
koog7 <k.ognev709@mail.com>
2024-06-29 14:20:58 +0600

    Добавлено тотображение нданных по категориям Ð, Ðдобавлены стили

--- src/containers/Quotes.tsx
+++ src/containers/Quotes.tsx
@@ -2,6 +2,7 @@ import Home from "./Home.tsx";
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import axiosAPI from '../axios/axiosLink.tsx'
+import {Button, Card, CardContent, Typography} from "@mui/material";
 
 interface FormData  {
     quoteCategory: string;
@@ -13,11 +14,6 @@ const Quotes = () => {
     const [quotes, setQuotes] = useState<FormData | null>(null)
     const {category} = useParams()
     useEffect(() => {
-
-        // let url = '/quotes.json';
-        // if (category) {
-        //     url += `?orderBy="quoteCategory"&equalTo="${category}"`;
-        // }
         if(category !== 'all'){
             axiosAPI.get(`/quotes.json?orderBy="quoteCategory"&equalTo="${category}"`)
                 .then(response => {
@@ -33,11 +29,35 @@ const Quotes = () => {
         }
     }, [category]);
     return (
-        <div>
+        <div style={{display:"flex", flexDirection: 'row'}}>
             <Home />
             <div>
-                <p>{quotes?.author}</p>
-                <p>{quotes?.quote}</p>
+                {quotes ? (
+                    <div>
+                        {Object.entries(quotes).map(([key, quote]) => (
+                            <div key={key} className="quote-card">
+                                <Card className="quote-card" style={{width:'500px' , marginLeft:'300px',marginTop:'20px', textAlign:'left',display:"flex", flexDirection:'row'}}>
+                                    <CardContent>
+                                        <Typography variant="body1" component="p">
+                                            "{quote.quote}"
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            - {quote.author}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardContent sx={{marginLeft:'auto',display:"flex", gap:'5px'}}>
+                                        <Button variant="outlined" sx={{ color: 'green', borderColor: 'green' }}>Edit</Button>
+                                        <Button variant="outlined" sx={{ color: 'red', borderColor: 'red' }}>Delete</Button>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
+                ):(
+                    <div>
+                        <h1>There is nothing...</h1>
+                    </div>
+                )}
             </div>
         </div>
     );
